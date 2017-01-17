287ced1683cd682c4317da5ad4f81e1116c3479e
aaron.brown <aaron.brown@267L>
2017-01-17 11:31:41 -0700

    clean up

--- src/main/java/com/stg/imageconsumer/domain/DatabaseConfiguration.java
+++ src/main/java/com/stg/imageconsumer/domain/DatabaseConfiguration.java
@@ -13,11 +13,15 @@ import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class DatabaseConfiguration {
 	@Bean
 	public DataSource dataSource() {
